397203e5db249ebd29f5f75bf532264f02a514ef
Anton Maryenkov <antonmaryenkov@gmail.com>
2020-08-01 14:41:41 +0700

    Исправил основные замечания

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,16 @@ let closeIcon = popup.querySelector('.popup__close-icon');
 let savePopupButton = popup.querySelector('.popup__button');
 let profileName = profile.querySelector('.profile__name');
 let profileJobs = profile.querySelector('.profile__jobs');
+let nameInput = popup.querySelector('#popup-name');
+let jobsInput = popup.querySelector('#popup-jobs');
 
-let popupToggle = function () {
+let popupToggle = function (event) {
+    event.preventDefault();
     popup.classList.toggle('popup_visible');
+    nameInput.value = profileName.textContent;
+    jobsInput.value = profileJobs.textContent;
 }
+
 
 let closePopup = function (event) {
     if (event.target !== event.currentTarget) return false;
@@ -17,15 +23,13 @@ let closePopup = function (event) {
 
 editButton.addEventListener('click', popupToggle);
 closeIcon.addEventListener('click', popupToggle);
-savePopupButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', closePopup);
 
 let formElement = popup.querySelector('.popup__container');
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let nameInput = popup.querySelector('#popup-name');
-    let jobsInput = popup.querySelector('#popup-jobs');
     profileName.textContent = nameInput.value;
     profileJobs.textContent = jobsInput.value;
+    popupToggle(event);
 }
 formElement.addEventListener('submit', formSubmitHandler);
